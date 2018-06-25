170c2837b1d34b6e9f0338af2ee2c4732e527890
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-06-25 16:43:21 +0530

    changed filename, added README

---
+++ README.md
@@ -0,0 +1,24 @@
+# spylapse
+
+To keep track of what happens when you leave your terminal
+
+# Requirements
+
+Requires `ffmpeg`
+
+### Ubuntu
+
+`sudo apt install ffmpeg`
+
+### Windows
+
+[FFmpeg](https://ffmpeg.org/download.html)
+
+Requires opencv
+
+`pip install -r requirements.txt`
+
+# Usage
+
+`python spylapse.py`
+
---
+++ spylapse.py
@@ -0,0 +1,26 @@
+from cv2 import *
+import time
+import os
+
+def takePicture(index):
+	cam = VideoCapture(0)
+	s, img = cam.read()
+	if s:
+		imwrite(os.path.join("img","t-{}.jpg".format(index)),img)
+i = 1
+
+try:
+	while(True):
+		# takePicture(int(time.time()))
+		# not using because glob (*) is not supported on Windows ffmpeg
+		takePicture(i)
+		i+=1
+		time.sleep(2)
+except:
+	pass
+finally:
+	print("Stitching together...")
+	os.system("ffmpeg -r 12 -i img/t-%d.jpg -c:v libx264 -y {}.mp4".format(time.strftime("%d-%m-%y.%I-%M")))
+	os.chdir("img")
+	for files in os.listdir():
+		os.remove(files)
